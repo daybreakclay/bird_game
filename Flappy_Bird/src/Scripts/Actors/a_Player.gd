89a3cbe5cf710d5a521b039aa67feebdb94a9fdd
daybreakclay <41237562+daybreakclay@users.noreply.github.com>
2020-04-07 07:41:25 -0500

    Remapped key binds

--- Flappy_Bird/src/Scripts/Actors/a_Player.gd
+++ Flappy_Bird/src/Scripts/Actors/a_Player.gd
@@ -2,15 +2,30 @@ extends Area2D
 var velocity = Vector2()
 
 func _physics_process(delta):
+	
 	if(Global.player_ready):
 		velocity.y += 50
 		position += velocity * delta
+
+
+func _process(delta):
+
+	if (velocity.y < 0):
+		rotation = velocity.y * (-delta / 7)
+	elif (velocity.y > 0):
+		rotation = velocity.y * (delta / 8)
+	else:
+		rotation = 0
+
+
 
 func _input(_event):
 	if (!Global.game_over):
 		if Input.is_action_just_pressed("jump"):
 			Global.player_ready = true
 			velocity.y = -900
+			
+			
 	else:
 		return
 
